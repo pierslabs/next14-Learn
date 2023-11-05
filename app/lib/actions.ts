85c9bd75e39a14d2cede1bd3db34a318b7e549df
Pedro Losas <pedrolosasp@gmail.com>
2023-11-05 01:21:11 +0100

    minor fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,7 +34,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-
+  console.log(validatedFields);
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
